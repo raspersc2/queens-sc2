845d8b1bf0aed57e283cf68d114809bf6d797bfc
DESKTOP-ABSCGQ6\Tom <tomkerrdev@gmail.com>
2021-03-24 10:20:30 +0000

    Improve inject queen control between injects

--- queens_sc2/inject.py
+++ queens_sc2/inject.py
@@ -33,16 +33,47 @@ class Inject(BaseUnit):
             elif self.policy.defend_against_ground and ground_threats_near_bases:
                 await self.do_queen_micro(unit, ground_threats_near_bases)
             else:
-                if unit.energy >= 25:
+                if unit.energy >= 25 and not th.has_buff():
                     unit(AbilityId.EFFECT_INJECTLARVA, th)
-                # regardless of policy, chase away enemy close to th
-                # but if queen gets too far away, walk back to th
-                elif unit.distance_to(th) > 7:
-                    unit.move(th.position)
-                elif self.bot.enemy_units.filter(
-                    lambda enemy: enemy.position.distance_to(th) < 10
-                ):
-                    unit.attack(self.find_closest_enemy(unit, self.bot.enemy_units))
+                # control the queen between injects
+                else:
+                    await self._control_inject_queen_near_base(
+                        air_threats_near_bases, ground_threats_near_bases, unit, th
+                    )
 
     def update_policy(self, policy: Policy) -> None:
         self.policy = policy
+
+    async def _control_inject_queen_near_base(
+        self,
+        air_threats_near_bases: Units,
+        ground_threats_near_bases: Units,
+        queen: Unit,
+        townhall: Unit,
+    ) -> None:
+        """
+        Between injects, we want the Queen to have the following behavior:
+        - Attack any enemy that gets too close
+        - Move the Queen back if she goes too far
+        - Stay out of the mineral line, incase bot has custom mineral gathering(don't block workers)
+        """
+        # stop queen wondering off is most important
+        if queen.distance_to(townhall) > 7:
+            queen.move(townhall)
+            return
+        close_threats: Units = Units([], self.bot)
+        # we can only have close threats if enemy are near our bases in the first place
+        if air_threats_near_bases or ground_threats_near_bases:
+            close_threats = self.bot.enemy_units.filter(
+                lambda enemy: enemy.position.distance_to(townhall) < 10
+            )
+
+        if close_threats:
+            await self.do_queen_micro(queen, close_threats)
+        # every now and then, check queen is not in the mineral field
+        elif self.bot.state.game_loop % 128 == 0:
+            close_mfs: Units = self.bot.mineral_field.filter(
+                lambda mf: mf.distance_to(townhall) < 8
+            )
+            if close_mfs and queen.distance_to(close_mfs.center) < 6:
+                queen.move(queen.position.towards(close_mfs.center, -1))
